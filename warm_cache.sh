2ce0eef22978c4d91dc0e2546ef306eb0b5ca366
KevinLmn <ikebilol@gmail.com>
2025-03-24 00:18:37 +0100

    change variables on warmup cache . sh

--- warm_cache.sh
+++ warm_cache.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-curl -X GET http://localhost:3005/api/latest /home/ikebi/manga-reader-front/warm_cache.sh/logfile.log 2>&1
-curl -X GET http://localhost:3005/api/popular /home/ikebi/manga-reader-front/warm_cache.sh/logfile.log 2>&1
+# Load .env variables
+export $(grep -v '^#' .env | xargs)
+
+curl -X GET "$NEXT_PUBLIC_FRONT_END_URL/api/latest" /home/ikebi/manga-reader-front/warm_cache.sh/logfile.log 2>&1
+curl -X GET "$NEXT_PUBLIC_FRONT_END_URL/api/popular" /home/ikebi/manga-reader-front/warm_cache.sh/logfile.log 2>&1
